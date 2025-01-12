377a1991b4df555c6ad15f398169f4f8a97a0453
Karan <karanlokchandani@protonmail.com>
2025-01-12 15:51:02 +0530

    markdown preprocessing init

--- backend/src/db/models.py
+++ backend/src/db/models.py
@@ -1,16 +1,14 @@
-from typing import List
 import sqlalchemy.dialects.postgresql as pg
 from typing import Optional
-from sqlmodel import SQLModel, Field, Relationship, Column
+from sqlmodel import SQLModel, Field, Column
 import uuid
 from datetime import datetime
 
 class PDFDocument(SQLModel, table=True):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()), primary_key=True)
     name: str = Field(max_length=255, nullable=False)
-    description: Optional[str] = Field(default=None, max_length=500)
     upload_date: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
-    file_path: str
+    file_content: str = Field(sa_column=Column(pg.TEXT, default=""))
 
     def __repr__(self) -> str:
         return f"<User {self.name}>"
