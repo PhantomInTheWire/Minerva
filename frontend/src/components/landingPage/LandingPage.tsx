53f50730bf75973999cb379d2a1f3e318980aa78
Ishu <kumarishu494@gmail.com>
2025-01-12 15:59:30 +0530

    Typewriter animation and avatar added

--- frontend/src/components/landingPage/LandingPage.tsx
+++ frontend/src/components/landingPage/LandingPage.tsx
@@ -6,63 +6,70 @@ import { Navbar } from "./Navbar";
 import { Button } from "../ui/button";
 // import { ContainerScroll } from "../ui/container-scroll-animation";
 import ScrollAnimatedContainer from "../ui/scroll-animated-container";
+import AvatarCircles from "../ui/avatar-circles";
+import { AVATARS, WORDS } from "./data";
+import { TypewriterEffect } from "../ui/typewriter-effect";
 
 export default function LandingPage() {
   return (
-      <div className="landing-page w-full min-h-screen flex flex-col">
-        <Navbar/>
+    <div className="landing-page w-full min-h-screen flex flex-col">
+      <Navbar />
 
-        <section
-            id="home"
-            className="hero w-full h-[60vh] flex flex-col items-center"
-        >
-          <div className="w-full h-full flex flex-col items-center justify-center gap-20">
-            <div className="flex flex-col">
-              <h1 className="text-[5rem] font-semibold">Learning your way.</h1>
-              <h2 className="text-xl text-center text-muted-foreground">
-                Learn through the scientifically proven methods with the help of
-                AI
-              </h2>
-            </div>
-            <div className="flex gap-6">
-              <Link
-                  href="#features"
-                  className="text-xl rounded-full px-6 py-2 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground transition-all duration-300"
-              >
-                See features
-              </Link>
-              <Button className="text-xl rounded-full px-6 py-6">
-                Get Started
-              </Button>
-            </div>
-            <AnimatedGridPattern
-                numSquares={30}
-                maxOpacity={0.1}
-                duration={3}
-                repeatDelay={1}
-                className={cn(
-                    "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
-                    "inset-x-0 inset-y-[-40%] h-[200%] skew-y-12 -z-10"
-                )}
-            />
+      <section
+        id="home"
+        className="hero w-full h-[80vh] flex flex-col items-center"
+      >
+        <div className="w-full h-full flex flex-col items-center justify-center gap-20">
+          <div className="flex flex-col">
+            {/* <h1 className="text-[5rem] font-semibold">Learning your way.</h1> */}
+            <TypewriterEffect words={WORDS} />
+            <h2 className="text-xl text-center text-muted-foreground">
+              Learn through the scientifically proven methods with the help of
+              AI
+            </h2>
           </div>
-        </section>
-        <section
-            id="features"
-            className="w-full min-h-screen flex flex-col items-center pt-20" // Added min-height and padding
-        >
-          <ScrollAnimatedContainer>
-            <Image
-                src={`/window.png`}
-                alt="hero"
-                width={1600}
-                height={781}
-                className="mx-auto rounded-2xl object-contain h-full object-left-top"
-                priority // Added priority loading
-                draggable={false}
-            />
-          </ScrollAnimatedContainer>
-        </section>
-      </div>
+          <div className="flex gap-6">
+            <Link
+              href="#features"
+              className="text-xl rounded-full px-6 py-2 border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground transition-all duration-300"
+            >
+              See features
+            </Link>
+            <Button className="text-xl rounded-full px-6 py-6">
+              Get Started
+            </Button>
+          </div>
+
+          <div className="flex flex-col items-center gap-2">
+            <AvatarCircles numPeople={999} avatarUrls={AVATARS} />
+            <p className="text-muted-foreground">Loved by 999+ learners</p>
+          </div>
+        </div>
+        <AnimatedGridPattern
+          numSquares={30}
+          maxOpacity={0.1}
+          duration={3}
+          repeatDelay={1}
+          className={cn(
+            "[mask-image:radial-gradient(500px_circle_at_center,white,transparent)]",
+            "inset-x-0 inset-y-[-40%] h-[200%] skew-y-12 -z-10"
+          )}
+        />
+      </section>
+      <section id="features" className="w-full flex flex-col items-center">
+        {/* <ContainerScroll titleComponent={<></>}> */}
+        <ScrollAnimatedContainer>
+          <Image
+            src={`/window.png`}
+            alt="hero"
+            width={1600}
+            height={781}
+            className="mx-auto rounded-2xl object-contain h-full object-left-top"
+            draggable={false}
+          />
+        </ScrollAnimatedContainer>
+        {/* </ContainerScroll> */}
+      </section>
+    </div>
   );
 }
---
+++ frontend/src/components/landingPage/data.ts
@@ -0,0 +1,45 @@
+export const WORDS = [
+  {
+    text: "Learning",
+  },
+  {
+    text: "your",
+  },
+  {
+    text: "way",
+  },
+  {
+    text: "with",
+  },
+  {
+    text: "AI.",
+    className: "text-blue-500 dark:text-blue-500",
+  },
+];
+
+export const AVATARS = [
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/135950363",
+    profileUrl: "https://github.com/PhantomInTheWire",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/47332922",
+    profileUrl: "https://github.com/ishu-codes",
+  },
+  {
+    imageUrl: "https://avatars.githubusercontent.com/u/146630018",
+    profileUrl: "https://github.com/R-priyanshu",
+  },
+  //   {
+  //     imageUrl: "https://avatars.githubusercontent.com/u/59228569",
+  //     profileUrl: "https://github.com/safethecode",
+  //   },
+  //   {
+  //     imageUrl: "https://avatars.githubusercontent.com/u/59442788",
+  //     profileUrl: "https://github.com/sanjay-mali",
+  //   },
+  //   {
+  //     imageUrl: "https://avatars.githubusercontent.com/u/89768406",
+  //     profileUrl: "https://github.com/itsarghyadas",
+  //   },
+];
