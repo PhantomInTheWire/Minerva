a9629e8f443b1e0b02c01423c374940b3883a024
Ishu <kumarishu494@gmail.com>
2025-01-14 14:40:07 +0530

    Glass effect added and old ui components removed

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,13 @@
+import { AnimatedGridPattern } from "./grid-patterns";
+import { AnimatedTooltip } from "./animated-tooltip";
+import { Button } from "./button";
+import { HeroSection } from "./hero-section";
+import ScrollAnimatedContainer from "./scroll-animated-container";
+
+export {
+  AnimatedGridPattern,
+  AnimatedTooltip,
+  Button,
+  HeroSection,
+  ScrollAnimatedContainer,
+};
